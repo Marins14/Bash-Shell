bd514253dd873a57a65725e509af6cc46ad32e79
Marins14 <matheus.bernardello@live.com>
2023-10-22 12:30:29 -0300

    Falta corrigir a parte comentada

--- Organizador/organizador.sh
+++ Organizador/organizador.sh
@@ -4,177 +4,194 @@
 #=======================================================#
 #			Organizador de Imagens/DOCs			      	#
 #=======================================================#
-# Função: Organizar imagens PNG,JPEG,JPG e documentos 	#
+# Função: Organizar Imagens PNG,JPEG,JPG e Documentos 	#
 # Autor: Matheus Marins Bernardello 				  	# 
 # Criado em: 2023/10/19                               	#
 # Att: --/--/--                                       	#
 #=======================================================#
 
 #Definição dos caminhos a serem utilizados ao longo do programa
-DATA=$(date +%d"/"%m"/"%y" - "%H":"%M) # exemplo de saida: 20/10/2023 - 13:43
+#DATA=$(date +%d"-"%m"-"%y"-"%H":"%M) # exemplo de saida: 20/10/2023 - 13:43
+DATA=$(date +"%d-%m-%y-%H:%M")
 
-dir_origem='/home/imagens' # Caso Linux 
-#dir_origem='/c/Users/%USERNAME%/imagens' # Caso windows
+dir_origem="/home/$(whoami)/Imagens" # Caso Linux 
+#dir_origem='/c/Users/%USERNAME%/Imagens' # Caso windows
 
-dir_destino='/home/imagens/Organizadas' # Caso linux
-#dir_destino='/c/Users/%USERNAME%/imagens/Organizadas' # Caso windows
+dir_destino="/home/$(whoami)/Imagens/Organizadas" # Caso linux
+#dir_destino='/c/Users/%USERNAME%/Imagens/Organizadas' # Caso windows
 
-dir_org_doc='/home/documentos' # Caso linux
-#dir_org_doc='/c/Users/%USERNAME%/documentos' # Caso windows
+dir_org_doc="/home/$(whoami)/Documentos" # Caso linux
+#dir_org_doc='/c/Users/%USERNAME%/Documentos' # Caso windows
 
-dir_dest_doc='/home/documentos/Organizado' # Caso linux
-#dir_dest_doc='/c/Users/%USERNAME%/documentos/Organizado' # Caso windows
+dir_dest_doc="/home/$(whoami)/Documentos/Organizado" # Caso linux
+#dir_dest_doc='/c/Users/%USERNAME%/Documentos/Organizado' # Caso windows
 
-dir_log='/home/documentos/log' # Caso Linux
-#dir_log='/c/Users/%USERNAME%/documentos/log' # Caso windows
+dir_log="/home/$(whoami)/Documentos/log" # Caso Linux
+#dir_log='/c/Users/%USERNAME%/Documentos/log' # Caso windows
 
-#Função para organizar as imagens
-func_organiza_img(){
-	echo "$USER escolheu organizar"
-	#Se não existir o diretório de log, ele irá criar
-	if [[ ! -d $dir_log ]]; then
+#Se não existir o diretório de log, ele irá criar
+if [[ ! -d $dir_log ]]; then
 		#Cria o diretório de log, em caso de erro ele irá informar
-		if mkdir -p $dir_log;then
+	if mkdir -p "$dir_log";then
 			echo "Diretório log criado em $dir_log"
-		else
+	else
 			echo "Erro ao criar o diretório de log, favor verificar!"
-		fi
+	fi
+fi	
+#Função para organizar as Imagens
+func_organiza_img() {
+    echo "$USER escolheu organizar"
+    # Caso a pasta destino não exista, ele cria
+    if [[ ! -d $dir_destino ]]; then
+        if mkdir -p "$dir_destino"; then
+            echo "Diretório de destino criado em $dir_destino"
+        fi
+    else
+        echo "Erro ao criar o diretório de destino, favor verificar!"
+    fi
 
-	fi
-	#Caso a pasta destino não exista, ele cria
-	if [[ ! -d $dir_destino ]];then 
-		if mkdir -p "$dir_destino";then
-			echo "Diretório de destino criado em $dir_destino"
-		fi
-		else
-			echo "Erro ao criar o diretório de destino, favor verificar!"
-	fi
-	
-	echo "Iniciando a organização!" 
-	#Faz a troca para a pasta de origem para inicar
-	cd "$dir_origem" || exit 
-	#Irá pegar todos os arquivos com a devida extensão e alocar na pasta definida
-	for arquivo in *; do
-		if [[ -f "$arquivo" ]];then
-			PNG="*.png"
-			mv "$PNG" "$dir_destino"
-			JPEG="*.jpeg"
-			mv "$JPEG" "$dir_destino"
-			JPG="*.jpg"
-			mv "$JPG" "$dir_destino"
-			echo "Finalizando..." 
-			sleep 1
-			echo "Suas imagens foram movidas com sucesso!" 
-			valida=`ls /$dir_destino`
-			echo $valida
-			echo "OBS: Lembrando que você encontrar tudo que foi movido para: $dir_destino"
-		else
-			echo "Não foram encontrados arquivos, favor verificar a existência dos mesmos"
-		fi
-	done
-} >> $dir_log/func_organiza__img_$DATA.log #Aloca todas as info do programa neste diretório 
+    echo "Iniciando a organização!"
 
-#Função para organizar os documentos
-func_organiza_doc(){
-	echo "$USER escolheu organizar"
-	#Se não existir o diretório de log, ele irá criar
-	if [[ ! -d $dir_log ]]; then
-		#Cria o diretório de log, em caso de erro ele irá informar
-		if mkdir -p $dir_log;then
-			echo "Diretório log criado em $dir_log"
-		else
-			echo "Erro ao criar o diretório de log, favor verificar!"
-		fi
+    # Faz a troca para a pasta de origem para iniciar
+    cd "$dir_origem" || exit
 
-	fi
-	#Caso a pasta destino não exista, ele cria
-	if [[ ! -d $dir_destino ]];then 
-		if mkdir -p "$dir_destino";then
-			echo "Diretório de destino criado em $dir_destino"
-		fi
-		else
-			echo "Erro ao criar o diretório de destino, favor verificar!"
-	fi
-	
-	echo "Iniciando a organização!" 
-	#Troca para a pasta de origem dos doc
-	cd "$dir_org_doc" || exit
-	#Faz a varredura dos arquivos na pasta de origem
-	for arquivo in *;
-	do 
-		if [[ -f $arquivo ]]; then
-			DOC="*.doc"
-			mv "$DOC" "$dir_dest_doc"
-			DOCX="*.docx"
-			mv "$DOCX" "$dir_dest_doc"
-			TXT="*.TXT"
-			mv "$TXT" "$dir_dest_doc"
-			PDF="*.pdf" 
-			mv "$PDF" "$dir_dest_doc"
-			echo "Finalizando..." 
-			sleep 1
-			echo "Seus arquivos foram movidas com sucesso!"
-			valida=`ls /$dir_dest_doc`
-			echo $valida
-			sleep 2
-			echo "OBS: Lembrando que você encontrar tudo que foi movido para: $dir_dest_doc"
-			sleep 1 
-			#Adicionando mais extensões caso necessário
-			echo "Sentiu falta de alguma extensão? (S/N)" 
-			read falta
-			falta=$( $falta | tr a-z A-Z)
-			#Se sim, ele irá perguntar quantas mais e quais são fazendo o devido tratamento
-			if [[ $falta == "S" ]]; then
-				echo "Ok, quantas mais você quer adicionar ? Lembrando só podem ser mais 2 (1-2)"
-				read qtde
-				#Verificando se a qtde é um numero válido
-				if ! [[ "$qtde" =~ ^[1-2]$ ]]; then
-        			echo "Erro: Por favor, insira 1 ou 2."
-        			exit 1
-    			fi
-				if [[ $qtde -eq 1 ]]; then
-				echo "Por favor nos diga qual a extensão do arquivo!"
-				read extensao
-				#Validando se a extensão é alfanumerica(por exemplo, sem espaços)
-				if ! [[ "$extensao" =~ ^[a-zA-Z0-9]+$ ]]; then
-					echo "Erro: Por favor, insira uma extensão válida."
-					exit 1
-				fi
-				EXT="*.$extensao"
-				mv "$EXT" "dir_dest_doc" 
-				echo "Feito!" 
-				elif [[ $falta -eq 2 ]]; then 
-				echo "Por favor nos diga quais as extensões dos arquivos!"
-				read extensao1
-				read extensao2
-				#Validando se a extensão é alfanumerica(por exemplo, sem espaços)
-				if [[ ! "$extensao1" =~ ^[a-zA-Z0-9]+$ || ! "$extensao2 " =~ ^[[:alnum:]]+$ ]]; then
-					echo "Erro: Por favor, insira uma extensão válida."
-					exit 1
-				fi
-				EXT="*.$extensao"
-				mv "$EXT" "dir_dest_doc" 
-				EXT1="*.$extensao2"
-				mv "$EXT1" "$dir_dest_doc"
-				echo "Feito!"
-				fi
-				valida=`ls /$dir_dest_doc`
-				echo $valida
-			else 
-				echo "Ótimo!" 
-			fi
-		else
-			echo "Não foram encontrados arquivos, favor verificar a existência dos mesmos"
-		fi
-	done
-} >> $dir_log/func_organiza_doc_$DATA.log #Aloca todas as info do programa neste diretório 
+    # Move os arquivos PNG para o diretório de destino
+    for arquivo in *.png; do
+        if [[ -f "$arquivo" ]]; then
+            mv "$arquivo" "$dir_destino"
+        fi
+    done
+
+    # Move os arquivos JPEG para o diretório de destino
+    for arquivo in *.jpeg; do
+        if [[ -f "$arquivo" ]]; then
+            mv "$arquivo" "$dir_destino"
+        fi
+    done
+
+    # Move os arquivos JPG para o diretório de destino
+    for arquivo in *.jpg; do
+        if [[ -f "$arquivo" ]]; then
+            mv "$arquivo" "$dir_destino"
+        fi
+    done
+
+    echo "Finalizando..."
+    echo "Suas Imagens foram movidas com sucesso!" 
+    valida=$(ls "$dir_destino")
+    echo "$valida"
+    echo "OBS: Lembrando que você encontrar tudo que foi movido para: $dir_destino"
+
+} >> "${dir_log}/func_organiza_img_${DATA}.log" 2>&1 # Aloca todas as info do programa neste diretório 
+
+# Função para organizar os Documentos
+func_organiza_doc() {
+    echo "$USER escolheu organizar"
+    
+    # Caso a pasta destino não exista, ele cria
+    if [[ ! -d $dir_dest_doc ]]; then
+        if mkdir -p "$dir_dest_doc"; then
+            echo "Diretório de destino criado em $dir_dest_doc"
+        fi
+    else
+        echo "Erro ao criar o diretório de destino, favor verificar!"
+    fi
+
+    echo "Iniciando a organização!" 
+    # Troca para a pasta de origem dos doc
+    cd "$dir_org_doc" || exit
+
+    # Faz a varredura dos arquivos na pasta de origem
+    for arquivo in *; do 
+        if [[ -f $arquivo ]]; then
+            DOC="*.doc"
+            mv "$DOC" "$dir_dest_doc"
+        fi
+    done
+
+    # Repita o processo para os outros tipos de documento (docx, TXT, pdf, etc.)
+    for arquivo in *.docx; do 
+        if [[ -f $arquivo ]]; then
+            mv "$arquivo" "$dir_dest_doc"
+        fi
+    done
+
+    for arquivo in *.txt; do 
+        if [[ -f $arquivo ]]; then
+            mv "$arquivo" "$dir_dest_doc"
+        fi
+    done
+
+    for arquivo in *.pdf; do 
+        if [[ -f $arquivo ]]; then
+            mv "$arquivo" "$dir_dest_doc"
+        fi
+    done
+
+    echo "Finalizando..." 
+    echo "Seus arquivos foram movidos com sucesso!"
+    valida=$(ls "$dir_dest_doc")
+    echo "$valida"
+    echo "OBS: Lembrando que você encontrar tudo que foi movido para: $dir_dest_doc"
+    
+    # Adicionando mais extensões caso necessário
+    # echo "Sentiu falta de alguma extensão? (S/N)" 
+    # read falta
+    # falta=$( echo "$falta" | tr a-z A-Z)
+    
+    # # Se sim, ele irá perguntar quantas mais e quais são fazendo o devido tratamento
+    # if [[ $falta == "S" ]]; then
+    #     echo "Ok, quantas mais você quer adicionar ? Lembrando só podem ser mais 2 (1-2)"
+    #     read qtde
+    #     # Verificando se a qtde é um numero válido
+    #     if ! [[ "$qtde" =~ ^[1-2]$ ]]; then
+    #         echo "Erro: Por favor, insira 1 ou 2."
+    #         exit 1
+    #     fi
+
+    #     if [[ $qtde -eq 1 ]]; then
+    #         echo "Por favor nos diga qual a extensão do arquivo!"
+    #         read extensao
+    #         # Validando se a extensão é alfanumerica (por exemplo, sem espaços)
+    #         if ! [[ "$extensao" =~ ^[a-zA-Z0-9]+$ ]]; then
+    #             echo "Erro: Por favor, insira uma extensão válida."
+    #             exit 1
+    #         fi
+
+    #         EXT="*.$extensao"
+    #         mv "$EXT" "$dir_dest_doc" 
+    #         echo "Feito!" 
+
+    #     elif [[ $qtde -eq 2 ]]; then 
+    #         echo "Por favor nos diga quais as extensões dos arquivos!"
+    #         read extensao1
+    #         read extensao2
+    #         # Validando se a extensão é alfanumerica (por exemplo, sem espaços)
+    #         if [[ ! "$extensao1" =~ ^[a-zA-Z0-9]+$ || ! "$extensao2 " =~ ^[[:alnum:]]+$ ]]; then
+    #             echo "Erro: Por favor, insira uma extensão válida."
+    #             exit 1
+    #         fi
+
+    #         EXT="*.$extensao1"
+    #         mv "$EXT" "$dir_dest_doc" 
+    #         EXT1="*.$extensao2"
+    #         mv "$EXT1" "$dir_dest_doc"
+
+    #         echo "Feito!"
+    #     fi
+    #     valida=$(ls "$dir_dest_doc")
+    #     echo "$valida"
+    # else 
+    #     echo "Ótimo!" 
+    # fi
+} >> "${dir_log}/func_organiza_doc_${DATA}.log" 2>&1 #Aloca todas as info do programa neste diretório 
 
 #Inicio do programa, perguntando se executa ou não
 echo "Olá! Bem vindo ao Organizador de arquivos, vamos lá ? (S/N)" 
 read escolha 
-escolha=$( escolha | tr a-z A-Z) #Garanto que o digitado pelo usuario seja tratado em MAIUSCULO
+escolha=$( echo $escolha | tr a-z A-Z ) #Garanto que o digitado pelo usuario seja tratado em MAIUSCULO
 
-#Se escolher Imagens, chama a função da mesma, o mesmo para documentos ou simplesmente sair
+#Se escolher Imagens, chama a função da mesma, o mesmo para Documentos ou simplesmente sair
 if [[ $escolha == "S" ]]; then
 	echo "Perfeito $USER, vamos lá, o que deseja organizar ? 
 	1 - Imagens 
@@ -201,10 +218,6 @@ if [[ $escolha == "S" ]]; then
 			echo "Saindo..." 
 			exit 1
 		;; 
-		*) 
-			echo "Revise sua escolha!"
-			exit 1
-		;;
 	esac
 else 
 	echo "Ok...saindo"
