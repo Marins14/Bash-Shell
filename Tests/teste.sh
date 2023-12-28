130b273e86e78494229e6e70b0528ea7c27dd714
Marins14 <matheus.bernardello@live.com>
2023-12-28 19:26:28 -0300

    BS: Add new script for test

---
+++ Tests/teste.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+echo "olá" 
+
+cd ~/Música
+
+
+if [ ! -d LinuxTeste  ]; then
+	mkdir LinuxTeste
+	echo "Criado" >> log.log
+else
+	echo "Apagado" >> log.log
+	rm -rf LinuxTeste
+
+fi
+
+echo "finalizado com sucesso"
+
