1b09377d3a75a145965c1111326aecf61e0c2ca2
Matheus <matheus.bernardelo@live.com>
2024-03-26 16:49:33 -0300

    fix(verifica_usuario): ajuste no condicional

--- verifica_usuario.sh
+++ verifica_usuario.sh
@@ -3,6 +3,24 @@
 #ALTERAR LINHA ABAIXO
 arquivo="/home/$(whoami)/Documentos/csv.csv"
 arquivo_saida=" "
+
+#Verifica se o arquivo existe
+if [[ ! -f $arquivo ]]; then
+	echo "Arquivo não encontrado"
+	read -p "Deseja criar? (s/n)" resposta
+	case $resposta in
+		s) touch $arquivo ;;
+		n) exit 1 ;;
+		*) echo "Opção inválida" ;;
+	esac
+	exit 1
+fi
+
+#Verifica se o arquivo de saída existe
+if [[ ! -f $arquivo_saida ]]; then
+	touch $arquivo_saida
+fi
+
 for i in $(cat $arquivo | cut -f3 | tail -n +3 ); do
 	#echo $i
 	net user /domain $i > $arquivo_saida
@@ -10,5 +28,3 @@ for i in $(cat $arquivo | cut -f3 | tail -n +3 ); do
 		#faz a verificação solicitada
 	done
 done
-
-
