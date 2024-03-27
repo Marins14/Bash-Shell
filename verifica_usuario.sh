94abac92fac28454097c8dd614202c57e3a62b7b
Matheus <matheus.bernardelo@live.com>
2024-03-26 21:42:32 -0300

    fix(verifica_usuario): ajuste de script

--- verifica_usuario.sh
+++ verifica_usuario.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-#ALTERAR LINHA ABAIXO
+#Script para verificar usuários em um arquivo .csv e verificar se os mesmos estão em um grupo específico, se não estiverem, gerar a lista que precisa ser adicionada
+
+#ALTERAR AS LINHAS ABAIXO
 arquivo="/home/$(whoami)/Documentos/csv.csv"
-arquivo_saida=" "
+arquivo_saida="/home/$(whoami)/Documentos/usuarios.txt"
+arquivo_validação="/home/$(whoami)/Documentos/validacao.txt"
 
 #Verifica se o arquivo existe
 if [[ ! -f $arquivo ]]; then
-	echo "Arquivo não encontrado"
-	read -p "Deseja criar? (s/n)" resposta
-	case $resposta in
-		s) touch $arquivo ;;
-		n) exit 1 ;;
-		*) echo "Opção inválida" ;;
-	esac
+	echo "Arquivo não encontrado, verifique o caminho"
 	exit 1
 fi
 
@@ -21,10 +18,22 @@ if [[ ! -f $arquivo_saida ]]; then
 	touch $arquivo_saida
 fi
 
-for i in $(cat $arquivo | cut -f3 | tail -n +3 ); do
-	#echo $i
-	net user /domain $i > $arquivo_saida
-	for usuario in $(cat $arquivo_saida | grep -i "alteração de senha" | awk '{print $2}'); do
+#Verifica se o arquivo está vazio, se não estiver, limpa o arquivo
+if [[ -s $arquivo_saida ]]; then
+	echo "O arquivo de saída não está vazio"
+	read -p "Deseja limpar o arquivo? [s/n]: " limpar
+	if [[ $limpar == "s" ]]; then
+		> $arquivo_saida
+	fi
+fi
+
+# Caso o arquivo não seja um .csv, ajustar a linha abaixo; 
+# Outra opção de cat é a: cat $arquivo| awk -F ';' '{print $3}' | sed 's/"//g' | tail -n +2
+for i in $(cat $arquivo | cut -d';' -f3 | sed 's/"//g' | tail -n +2); do
+	#echo $i >> $arquivo_saida
+	net user /domain $i >> $arquivo_saida
+	for grupos in $(cat $arquivo_saida | grep -i "texto que se refere aos grupos" | awk '{print $2}' ); do
 		#faz a verificação solicitada
+		echo "Usuário, possui os seguinte grupos associados:  $grupos" >> $arquivo_validação
 	done
 done
