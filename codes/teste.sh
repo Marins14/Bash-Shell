b84d460bf203358a596254c02571b072e68fbc92
Matheus <matheus.bernardelo@live.com>
2024-04-05 17:25:03 -0300

    Teste de commit automatico

---
+++ codes/teste.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+
+git add .
+
+echo "Digite a mensagem do commit: "
+read mensagem
+
+git commit -m "$mensagem"
+
+echo -p "Posso subir? (s/n)" choice
+
+if [ $choice == "s" ]; then
+    git push
+else
+    echo "Operação cancelada"
+fi
